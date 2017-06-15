fc13ae526413eedac30e72e291bb0f0eb30de7d2
craigambrose <craig@craigambrose.com>
2017-06-15 18:09:01 +0900

    upgrade Counter example to RN 0.45.1

--- src/Native/NativeUi/Elements.js
+++ src/Native/NativeUi/Elements.js
@@ -1,7 +1,4 @@
 const _ohanhi$elm_native_ui$Native_NativeUi_Elements = function () {
   return {
-    navigationCardStack: require("NavigationCardStack"),
-    navigationHeader: require("NavigationHeader"),
-    navigationHeaderTitle: require("NavigationHeaderTitle"),
   };
 }();
